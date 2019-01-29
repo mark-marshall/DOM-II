e1aea69c4f1861a13452ea04dc18a981933b6bd6
mark-marshall <mark.marshallgp@gmail.com>
2019-01-29 12:45:18 +0000

    [MVP] Event1: Added random nav colors with mouseover on hero image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+/* 	* [ ] `mouseover`
+ * [ ] `keydown`
+ * [ ] `wheel`
+ * [ ] `drag / drop`
+ * [ ] `load`
+ * [ ] `focus`
+ * [ ] `resize`
+ * [ ] `scroll`
+ * [ ] `select`
+ * [ ] `dblclick`
+ */
+
+// Replicating randomise color nav from WEB16 Class video
+const heroImage = document.querySelector('header img');
+const navItems = document.querySelectorAll('nav a');
+
+function getColor() {
+  const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomise() {
+  navItems.forEach(item => (item.style.color = getColor()));
+}
+
+heroImage.addEventListener("mousemove", randomise);
