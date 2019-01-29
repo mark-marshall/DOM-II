390c6a155148374a63d8b9df79b670d27b36fb63
mark-marshall <mark.marshallgp@gmail.com>
2019-01-29 12:59:07 +0000

    [MVP] Event2: Added scroll to page bottom on doubleclick of Let's Go header

--- js/index.js
+++ js/index.js
@@ -12,12 +12,12 @@
  */
 
 // Replicating randomise color nav from WEB16 Class video
-const heroImage = document.querySelector('header img');
-const navItems = document.querySelectorAll('nav a');
+const heroImage = document.querySelector("header img");
+const navItems = document.querySelectorAll("nav a");
 
 function getColor() {
-  const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-  let color = '#';
+  const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -29,3 +29,12 @@ function randomise() {
 }
 
 heroImage.addEventListener("mousemove", randomise);
+
+const letsGo = document.querySelector(".text-content h2");
+
+// Scroll to the botton on dblclick of "Let's Go Header"
+function pageBottom() {
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+letsGo.addEventListener('dblclick', pageBottom);
