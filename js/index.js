5df8aa4a5a0a1810eb1e036f15fba4665bb81fa0
mark-marshall <mark.marshallgp@gmail.com>
2019-01-29 13:21:13 +0000

    [MVP] Event3: Added image opacity on hover of Let's Go image

--- js/index.js
+++ js/index.js
@@ -12,12 +12,12 @@
  */
 
 // Replicating randomise color nav from WEB16 Class video
-const heroImage = document.querySelector("header img");
-const navItems = document.querySelectorAll("nav a");
+const heroImage = document.querySelector('header img');
+const navItems = document.querySelectorAll('nav a');
 
 function getColor() {
-  const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-  let color = "#";
+  const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  let color = '#';
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -28,13 +28,22 @@ function randomise() {
   navItems.forEach(item => (item.style.color = getColor()));
 }
 
-heroImage.addEventListener("mousemove", randomise);
-
-const letsGo = document.querySelector(".text-content h2");
+heroImage.addEventListener('mousemove', randomise);
 
 // Scroll to the botton on dblclick of "Let's Go Header"
+const letsGo = document.querySelector('.text-content h2');
+
 function pageBottom() {
   window.scrollTo(0, document.body.scrollHeight);
 }
 
 letsGo.addEventListener('dblclick', pageBottom);
+
+// Add image opacity on hover
+const image1 = document.querySelector('.img-one');
+
+function enlarge() {
+  image1.style.opacity = '0.5';
+}
+
+image1.addEventListener('mouseover', enlarge);
